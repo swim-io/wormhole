aa9e985fb781d44a9a22ff5e8381ffe7d844aa37
wormat <wormat@maelstrom.finance>
2021-10-06 16:34:11 +0200

    wormat: Add Ethereum token deployments in preparation for BSC

--- ethereum/scripts/deploy_swim_tokens.js
+++ ethereum/scripts/deploy_swim_tokens.js
@@ -8,7 +8,8 @@ module.exports = async function(callback) {
   try {
     const accounts = await web3.eth.getAccounts();
 
-    usdc = await TokenImplementation.new();
+    // Ethereum
+    const usdc = await TokenImplementation.new();
     await usdc.initialize(
       "USD Coin",
       "USDC",
@@ -18,7 +19,7 @@ module.exports = async function(callback) {
       0,
       "0x00"
     );
-    usdt = await TokenImplementation.new();
+    const usdt = await TokenImplementation.new();
     await usdt.initialize(
       "Tether USD",
       "USDT",
@@ -35,6 +36,35 @@ module.exports = async function(callback) {
     // mint 1000 units of each
     await usdc.mint(accounts[0], "1000000000000000000000")
     await usdt.mint(accounts[0], "1000000000000000000000")
+
+    // BSC
+    const busd = await TokenImplementation.new();
+    await busd.initialize(
+      "Binance-Peg BUSD Token",
+      "BUSD",
+      18,
+      0,
+      accounts[0],
+      0,
+      "0x00"
+    );
+    const bscUsd = await TokenImplementation.new(); // USDT on BSC
+    await bscUsd.initialize(
+      "Binance-Peg BSC-USD",
+      "BSC-USD",
+      18,
+      0,
+      accounts[0],
+      0,
+      "0x00"
+    );
+
+    console.log("BUSD deployed at: " + busd.address);
+    console.log("BSC-USD (USDT on BSC) deployed at: " + bscUsd.address);
+
+    // mint 1000 units of each
+    await busd.mint(accounts[0], "1000000000000000000000")
+    await bscUsd.mint(accounts[0], "1000000000000000000000")
 
     callback();
   } catch (e) {
