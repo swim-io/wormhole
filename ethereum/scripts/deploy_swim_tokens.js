a49ba21426df3978d03e5c5f35e874d0b86b85ce
wormat <wormat@maelstrom.finance>
2021-10-06 15:46:34 +0200

    wormat: Make token deployments match USDC/USDT

--- ethereum/scripts/deploy_swim_tokens.js
+++ ethereum/scripts/deploy_swim_tokens.js
@@ -7,35 +7,34 @@ const TokenImplementation = artifacts.require("TokenImplementation");
 module.exports = async function(callback) {
   try {
     const accounts = await web3.eth.getAccounts();
-    const decimals = 6;
 
-    tk1 = await TokenImplementation.new();
-    await tk1.initialize(
-      "TestFrom",
-      "FROM",
-      decimals,
+    usdc = await TokenImplementation.new();
+    await usdc.initialize(
+      "USD Coin",
+      "USDC",
+      6,
       0,
       accounts[0],
       0,
       "0x00"
     );
-    tk2 = await TokenImplementation.new();
-    await tk2.initialize(
-      "TestTo",
-      "TO",
-      decimals,
+    usdt = await TokenImplementation.new();
+    await usdt.initialize(
+      "Tether USD",
+      "USDT",
+      6,
       0,
       accounts[0],
       0,
       "0x00"
     );
 
-    console.log("TK1 deployed at: " + tk1.address);
-    console.log("TK2 deployed at: " + tk2.address);
+    console.log("USDC deployed at: " + usdc.address);
+    console.log("USDT deployed at: " + usdt.address);
 
     // mint 1000 units of each
-    await tk1.mint(accounts[0], "1000000000000000000000")
-    await tk2.mint(accounts[0], "1000000000000000000000")
+    await usdc.mint(accounts[0], "1000000000000000000000")
+    await usdt.mint(accounts[0], "1000000000000000000000")
 
     callback();
   } catch (e) {
